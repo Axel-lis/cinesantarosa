3e4df2d67d2dd55c03f246538d54bd9e2126a575
Axel Liscovsky <axelliscovsky@outlook.com.ar>
2024-09-23 16:07:46 -0300

    seccion compra fixed

--- compra.php
+++ compra.php
@@ -8,8 +8,8 @@
     <!-- Proceso de Compra -->
     <div class="container container-section-compra">
         <div class="proceso-compra">
-            <h2 class="fw-bolder mt-2">PROCESO DE COMPRA</h2>
-            <img src="./assets/img/sticker-popcorn.png" alt="Popcorn" class="small-icon">
+            <h2 class="fw-bolder my-5">PROCESO DE COMPRA</h2>
+            <img src="./assets/img/sticker-popcorn.png" alt="Popcorn" class="small-icon my-5">
         </div>
 
         <!-- Productos -->
@@ -38,18 +38,22 @@
 
         <!-- Imagen Compra -->
         <div class="proceso-compra mt-5">
-            <img src="./assets/img/sticker-ticket.png" alt="Ticket" class="small-icon-3 mb-4">
+            <img src="./assets/img/sticker-ticket.png" alt="Ticket" class="small-icon-3 my-5">
         </div>
         <!-- Ticket Compra -->
         <div class="row">
             <img src="./assets/img/COMPRA_Ticket-web.png" alt="Cine Millenium" class="img-fluid ticket-web">
             <img src="./assets/img/COMPRA_Ticket-Movil.png" alt="Cine Millenium" class="img-fluid ticket-movil">
         </div>
+        <br>
+        <br>
+        <br>
         <!-- Detalle de Compra -->
-        <div class="row">
-            <div class="col-md-6">
+        <div class="row my-5">
+            <div class="col-md-5 cantidad-entradas-container">
                 <div class="mb-3">
-                    <label for="cantidadEntradas" class="form-label mt-4 ">Cantidad de Entradas</label>
+                    <label for="cantidadEntradas" class="form-label mt-3 h3">Cantidad de
+                        Entradas</label>
                     <select class="form-select" id="cantidadEntradas">
                         <option>1 entrada</option>
                         <option>2 entradas</option>
@@ -58,9 +62,9 @@
                     </select>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 datos-personales-container">
                 <div class="mb-3">
-                    <label for="nombre" class="form-label text-white fw-bold mt-3 h3">Datos Personales</label>
+                    <label for="nombre" class="form-label mt-3 h3">Datos Personales</label>
                     <div class="row">
                         <div class="col-6">
                             <input type="text" class="form-control" id="nombre" placeholder="Nombre">
@@ -80,11 +84,11 @@
                 </div>
             </div>
         </div>
-
+        <br><br><br>
         <!-- Detalle de Compra -->
-        <div class="detalle-compra">
-            <h3 class="text-white text-center fw-bolder my-4">DETALLE DE COMPRA</h3>
-            <div class="proceso-compra">
+        <div class="detalle-compra mt-5">
+            <h3 class="text-white text-center fw-bolder my-5">DETALLE DE COMPRA</h3>
+            <div class="proceso-compra my-5">
                 <img src="./assets/img/sticker-movie.png" alt="Movie" class="small-icon-2 mb-4">
             </div>
 
@@ -114,7 +118,7 @@
                     </tr>
                 </tbody>
             </table>
-            <div class="text-center w-100" style="background-color: #fff; padding: 10px;">
+            <div class="text-center w-100" style="background-color: #fff; padding: 10px; margin-top:-15px;">
                 <h4 class="text-secondary">TOTAL: $3.877,50</h4>
                 <button class="btn btn-secondary">PREPARAR PAGO</button>
             </div>
